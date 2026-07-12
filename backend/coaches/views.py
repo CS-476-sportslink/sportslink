7937b288c8c1f195461f5ecaef6c9e69d5298299
TynanGuenther <tgunther701@gmail.com>
2026-07-12 15:08:43 -0600

    started coaches views and made get my profile view

--- backend/coaches/views.py
+++ backend/coaches/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import CoachProfile
+from .serializers import CoachProfileSerializer
 
-# Create your views here.
+#GET /api/coaches/me/
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_my_profile(request):
+    try:
+        profile = CoachProfile.objects.get(user=request.user)
+        serializer = CoachProfileSerializer(profile)
+        return Response(serializer.data)
+    except CoachProfile.DoesNotExist:
+        return Response({'error': 'Profile not found'}, status=status.HTTP_404_NOT_FOUND)
+
