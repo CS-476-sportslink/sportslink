6c0cef11194c502fd27d1cbe5d40ebf2bc13d0ac
TynanGuenther <tgunther701@gmail.com>
2026-07-14 19:46:14 -0600

    added connections/views.py and created list_connections view

--- backend/connections/views.py
+++ backend/connections/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Connection
+from .serializers import ConnectionSerializer
+from users.models import User
 
-# Create your views here.
+#GET /api/connections/
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_connections(request):
+    status_filter = request.query_params.get('status', None)
+
+    connections = Connection.objects.filter(
+        initiator=request.user
+    ) | Connection.objects.filter(
+        receiver=request.user
+    )
+
+    if status_filter:
+        connections = connections.filter(status=status_filter)
+
+    connections = connections.order_by('-created_at')
+    serializer = ConnectionSerializer(connections, many=True)
+    return Response(serializer.data)
+
