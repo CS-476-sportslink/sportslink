53dc0f0252b016bbac299ced12ae0b93308cfe81
TynanGuenther <tgunther701@gmail.com>
2026-07-14 18:12:00 -0600

    made update profile view

--- backend/coaches/views.py
+++ backend/coaches/views.py
@@ -39,3 +39,18 @@ def get_profile(request, pk):
     except CoachProfile.DoesNotExist:
         return Response({'error': 'Profile not found'}, status=status.HTTP_404_NOT_FOUND)
 
+#PUT /api/coaches/:id/update/
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_profile(request, pk):
+    try:
+        profile = CoachProfile.objects.get(pk=pk)
+        if profile.user != request.user:
+            return Response({'error': 'Not authorized'}, status=status.HTTP_403_FORBIDDEN)
+        serializer = CoachProfileSerializer(profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except CoachProfile.DoesNotExist:
+        return Response({'error': 'Profile not found'}, status=status.HTTP_404_NOT_FOUND)
