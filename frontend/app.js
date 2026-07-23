00823ef72cd5e5b736fca225df0ae90e175c4824
j12345232 <jpetry535@gmail.com>
2026-07-23 01:24:09 -0600

    started listeners for likes in app.js, also added link to liked posts on home.html

--- frontend/app.js
+++ frontend/app.js
@@ -322,3 +322,56 @@ function shareListeners() {
         });
     });
 }
+
+/* creating function for like listeners, will be similar to saving function above*/
+
+function likeListeners() {
+    document.querySelectorAll('.sl-like-btn').forEach(function(btn) {
+        btn.addEventListener('click', function() { //add click listener to the like button
+            const postId = btn.getAttribute('data-post-id'); // grab the post id from the button
+            const token = localStorage.getItem('access_token');
+            const icon = btn.querySelector('i');
+            const label = btn.querySelector('.sl-save-label') // Saved and saved text
+            if (btn.classList.contains('saved')) {
+                //unlike post
+                fetch(`http://127.0.0.1:8000/api/posts/${postId}/save/`, {
+                    method: 'DELETE',
+                    headers: { 
+                        'Authorization': 'Bearer ' + token 
+                    } // who am i
+                })
+                .then(function() {
+                    //change the button so it doesnt show saved state anymore
+                    btn.classList.remove('saved');
+                    icon.classList.remove('bi-bookmark-fill');
+                    icon.classList.add('bi-bookmark');
+                    label.textContent = 'Save';
+                })
+                .catch(function() {
+                    alert('Failed to unsave post');
+                });
+            } else {
+                //when the post is not saved, send request to backend to save post.
+                fetch(`http://127.0.0.1:8000/api/posts/${postId}/save/`, {
+                    method: 'POST',
+                    headers: { 
+                        'Authorization': 'Bearer ' + token 
+                    } // who am i
+                })
+                .then(function(response) { return response.json(); }) //convert response into javascript object
+                .then(function(result) {
+                    if (result.message) {
+                        //show button is in saved state
+                        btn.classList.add('saved');
+                        icon.classList.remove('bi-bookmark');
+                        icon.classList.add('bi-bookmark-fill');
+                        label.textContent = 'Saved';
+                    }
+                })
+                .catch(function() {
+                    alert('Failed to save post');
+                });
+            }
+        });
+    });    
+}
