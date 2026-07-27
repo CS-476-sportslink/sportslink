b7a786ecd0c5d5de69ddd311ac2d2494add9958f
TynanGuenther <tgunther701@gmail.com>
2026-07-27 14:53:15 -0600

    CORS changed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,6 +115,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
