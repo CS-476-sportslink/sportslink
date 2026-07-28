5530320f19c49ed4c28d2fd1e477b1f338733ce4
aria880 <arisha.m143@gmail.com>
2026-07-27 19:36:06 -0600

    configure password reset and forgot routing

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -36,5 +36,7 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/coaches/', include('coaches.urls')),
     path('api/posts/', include('posts.urls')),
+    path('api/users/', include('users.urls')),
+    
 
 ]
