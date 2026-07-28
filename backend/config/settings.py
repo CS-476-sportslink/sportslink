13b65cb0d27c0af5d9ec212423a6e4e63fce711d
aria880 <arisha.m143@gmail.com>
2026-07-27 19:33:57 -0600

    configure forgot and reset password settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -116,4 +116,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+
 ]
+
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
+#SMPT Configuration
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT',587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
